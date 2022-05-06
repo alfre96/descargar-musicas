0c5190928e2573a7c235e9be9b0f2120e7ffe4c8
alfre96 <53201697+alfre96@users.noreply.github.com>
2022-05-05 23:23:58 -0400

    descargar musica //desde PARAGUAY
    sirve para descargar musicas gratis

---
+++ descargar.py
@@ -0,0 +1,20 @@
+import os
+import webbrowser
+
+
+os.system("clear")
+os.system("date")
+print("""
+*******************************************************************************>
+*************************************************ALFREDO OCAMPOS***************>
+*******************************************************************************>
+""")
+
+#software libre #
+
+ingrese=input("ingrese nombre de la muisca: ")
+
+###############################################################################>
+while ingrese == "" or ingrese== " ": 
+        print("nombre  no valido")
+        ingrese=input("ingrese nombre de la muisca: ")
